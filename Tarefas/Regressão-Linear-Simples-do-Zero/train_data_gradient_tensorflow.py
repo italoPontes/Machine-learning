9cd6ea87a76decd4a5685bc88f04601ab0a93d97
italoPontes <italooliveira@copin.ufcg.edu.br>
2017-09-14 17:41:39 -0300

    Tensorflow version

--- Tarefas/Regressão-Linear-Simples-do-Zero/train_data_gradient_tensorflow.py
+++ Tarefas/Regressão-Linear-Simples-do-Zero/train_data_gradient_tensorflow.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from numpy import *
 
 #w0: -39.4462566791
 #w1: 5.59948287412
@@ -27,6 +26,7 @@ x_income = [ 10. , 10.40133779, 10.84280936, 11.24414716, 11.64548495, 12.086956
 y_income = [ 26.65883878, 27.30643535, 22.13241017, 21.1698405 , 15.19263352, 26.39895104, 17.43530658, 25.50788523, 36.88459469, 39.66610875, 34.39628056, 41.49799354, 44.98157487, 47.03959526, 48.25257829, 57.03425134, 51.49091921, 61.33662055, 57.58198818, 68.55371402, 64.3109253 , 68.95900864, 74.61463928, 71.8671953 , 76.09813538, 75.77521803, 72.48605532, 77.35502057, 72.11879045, 80.2605705 ]
 
 '''
+# Traning based on iterations number
 iter_number = 20000
 for i in range(iter_number):
 	sess.run(train, {x: x_income, y: y_income})
@@ -36,18 +36,26 @@ print("w0:", sess.run(w0))
 print("w1:", sess.run(w1))
 '''
 
-norma = 1 
-while norma > 0.0001:
+# Training based on gradient size
+norma = 1.0
+threshold = 0.001
+
+while( norma > threshold ):
 	w0_initial, w1_initial = sess.run([w0, w1], {x: x_income, y: y_income})
 	sess.run(train, {x: x_income, y: y_income})
 	w0_final, w1_final = sess.run([w0, w1], {x: x_income, y: y_income})
-	norma = sqrt((w0_final-w0_initial)**2 + (w1_final-w1_initial)**2)
+	norma_tf = tf.sqrt((w0_final-w0_initial)**2 + (w1_final-w1_initial)**2)
+	norma = float(sess.run(norma_tf))
 
-
-curr_w0, curr_w1, curr_loss = sess.run([w0, w1, loss], {x: x_income, y: y_income})
-print("w0: %s w1: %s loss: %s" % (curr_w0, curr_w1, curr_loss))
+# Training based on closed equation
+'''
+x_mean = numpy.mean(x)
+y_mean = numpy.mean(y)
+w1 = sum((x - x_mean)*(y - y_mean))/sum((x - x_mean)**2)
+w0 = y_mean-(w1*x_mean)
+return [w0, w1]
+'''
 
 # evaluate training accuracy
-print(sess.run(loss, {x: [x_income], y: [y_income]}))
-print("w0:", sess.run(w0))
-print("w1:", sess.run(w1))
+curr_w0, curr_w1, curr_loss = sess.run([w0, w1, loss], {x: x_income, y: y_income})
+print("w0: %s w1: %s loss: %s" % (curr_w0, curr_w1, curr_loss))
