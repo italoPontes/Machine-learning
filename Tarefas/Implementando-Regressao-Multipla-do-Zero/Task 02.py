c4c709c7e16cc0127734262583f3eb011d26448a
italoPontes <italooliveira@copin.ufcg.edu.br>
2017-10-02 10:45:27 -0300

    My version of Multiple Linear Regression

--- Tarefas/Implementando-Regressao-Multipla-do-Zero/Task 02.py
+++ Tarefas/Implementando-Regressao-Multipla-do-Zero/Task 02.py
@@ -7,6 +7,7 @@
 
 """This project will calculate Multiple Linear Regression
 """
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -17,15 +18,13 @@ import sys
 #  @param argv Is the parameter list passed
 def printErrorLog(argv):
 	message = "\n\n\n"
-	message += "This application works one of the ways:\n\n"
-	message += "python " + sys.argv[0] + " <input_file_name>\n"
-	message += "python " + sys.argv[0] + " <input_file_name> <output_figure_name> <learning_rate>\n"
-	message += "python " + sys.argv[0] + " <input_file_name> <output_figure_name> <learning_rate> <num_iterations>\n\n"
+	message += "Call this applications as:\n\n"
+	message += "python " + sys.argv[0] + " <input_file_name> <learning_rate> <stop_criteria> <output_prefix_filename>\n"
 	message += "in which:\n"
 	message += "<input_file_name>: Is the file name that contains the 'x' and 'y' values separated by the comma to be trained.\n"
-	message += "<output_figure_name>: Figure name to save iteraction x RSS graphic.\n"
-	message += "<learning_rate>: The rate in which the gradient will be changed in one step.\n"
-	message += "<num_iterations>: Interactions number that the slope line will approximate before a stop.\n"
+	message += "<learning_rate>: Learning rate value (e.g., 0.00003).\n"
+	message += "<stop_criteria>: Gradient norma size to stop the training (e.g., 0.000001).\n"
+	message += "<output_prefix_filename>: Graphics filename to save output.\n"
 	message += "\n\n\n"
 	print(message)
 
@@ -43,6 +42,12 @@ def save_figure(data, xlabel, ylabel, filename):
 	plt.clf() # clears the entire current figure 
 	plt.close() # closes a window
 	
+def normalize(mat):
+	max_values = np.amax( mat, axis = 0 )
+	min_values = np.amin( mat, axis = 0 )
+	diff = max_values - min_values
+	mat_normalized = ( mat - min_values ) / diff
+	return [mat_normalized, max_values, min_values]
 
 def compute_RSS(H, w, y):
 	mat = y - np.dot(H, w)
@@ -70,6 +75,7 @@ def gradient_descent(H, y, learning_rate, epsilon):
 	w = np.zeros((H.shape[1])) #has the same size of output
 	rss_total = []
 	rss_by_step = 0
+	norma_total = []
 	norma = epsilon+1
 	num_iterations = 0
 	
@@ -79,32 +85,38 @@ def gradient_descent(H, y, learning_rate, epsilon):
 		if num_iterations % 10 == 0:
 			rss_by_step = compute_RSS(H, w, y)
 			rss_total.append(rss_by_step)
+		norma_total.append(norma)
 	
-	return [w, num_iterations, rss_total]
+	return [w, num_iterations, rss_total, norma_total]
  
  
 
 ## Main function
 if __name__ == '__main__':
-	'''
-	if (len(sys.argv) != 2) and len(sys.argv) != 4 and (len(sys.argv) != 5):
+	if len(sys.argv) != 5:
 		printErrorLog(sys.argv)
 		exit(0)
-	'''
+
 	# Variable declaration	
 	input_filename = sys.argv[1]
 	learning_rate = float(sys.argv[2])
 	epsilon = float(sys.argv[3])
-	output_filename = sys.argv[4]
+	prefix_filename = sys.argv[4]
 	
 	att = genfromtxt(input_filename, delimiter=",", skip_header=1)
-	H = att[:,0:-1]
-	y = att[:,-1]
+	H = att[:,0:-1] # Get content to be trained
+	y = att[:,-1] # Get column of predict variable
+	H_with_ones = np.c_[np.ones(len(H)), H]
 	
-	H = np.c_[np.ones(len(H)), H]
+	[w, num_iterations, rss_total, norma_total] = gradient_descent(H_with_ones, y, learning_rate, epsilon)
 	
-	[w, num_iterations, rss_total] = gradient_descent(H, y, learning_rate, epsilon)
+	print("\n\nNum iterations: {0}\nRSS: {1}\nW: {2}".format(num_iterations, rss_total[-1], w))
 	
-	print("Num iterations: {0}\nCoefficients: {1}".format(num_iterations, w))
+	# Computing the same values with Scikit-learn
+	reg = LinearRegression()
+	reg.fit(H, y)
+	print("\nCoef with scikit-learn: {0}".format(reg.coef_))
+	print("Intercept with scikit-learn: {0}\n".format(reg.intercept_))
 	
-	save_figure(rss_total, "Iteration", "RSS", output_filename)
+	save_figure(rss_total, "Iteration", "RSS", prefix_filename + "_rss.png" )
+	save_figure(norma_total, "Iteration", "Norma", prefix_filename + "_norma.png" )	
